ac0c48b831172cac554c6079a8d516348c1d25e5
harriet.waninge <harriet.waninge%40alten.nl@git.alten.nl>
2025-05-09 15:23:34 +0200

    feat: test 1 up and running

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,9 +1,26 @@
-const chai = require('chai');
+const chai = require("chai");
 const assert = chai.assert;
 
-const Solver = require('../controllers/sudoku-solver.js');
-let solver;
+const Solver = require("../controllers/sudoku-solver.js");
+let solver = new Solver();
 
-suite('Unit Tests', () => {
-
+suite("Unit Tests", () => {
+  suite("string validation", () => {
+    test("Logic handles a puzzle string with invalid characters (not 1-9 or .)", () => {
+      assert.equal(solver.validate("hejjjjjj"), "");
+    });
+  });
 });
+
+/**Logic handles a valid puzzle string of 81 characters
+Logic handles a puzzle string with invalid characters (not 1-9 or .)
+Logic handles a puzzle string that is not 81 characters in length
+Logic handles a valid row placement
+Logic handles an invalid row placement
+Logic handles a valid column placement
+Logic handles an invalid column placement
+Logic handles a valid region (3x3 grid) placement
+Logic handles an invalid region (3x3 grid) placement
+Valid puzzle strings pass the solver
+Invalid puzzle strings fail the solver
+Solver returns the expected solution for an incomplete puzzle */
