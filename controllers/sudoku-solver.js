583c0f37586369fe23c8d427e35737eef1eb712e
harriet.waninge <harriet.waninge@gmail.com>
2025-05-13 12:18:28 +0200

    feat: column placement tests passing

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,8 +1,14 @@
 class SudokuSolver {
+  constructor() {
+    this.numberOfColums = 9;
+    this.numberOfRows = 9;
+    this.numberOfCharacters = this.numberOfColums * this.numberOfRows;
+  }
+
   validate(puzzleString) {
     let regEx = /[^1-9\.]/;
     let isInvalid =
-      puzzleString.length != 81
+      puzzleString.length != this.numberOfCharacters
         ? true
         : regEx.test(puzzleString)
         ? true
@@ -11,11 +17,39 @@ class SudokuSolver {
     return isInvalid ? "NO!" : "";
   }
 
-  checkRowPlacement(puzzleString, row, column, value) {}
+  checkRowPlacement(puzzleString, row, column, value) {
+    let targetRow = this.getTargetRow(puzzleString, row);
+    let isPresent = targetRow.includes(value);
+    return !isPresent;
+  }
+  getTargetRow(puzzleString, row) {
+    const rowLetters = "abcdefghijklmnopqrstuvwxyz";
+    let regex = new RegExp(`${row}`, "i");
+    let numberEnd = this.numberOfRows * (rowLetters.match(regex).index + 1);
+    let numberStart = numberEnd - this.numberOfRows;
+    let targetRow = puzzleString.substring(numberStart, numberEnd);
+    return targetRow;
+  }
 
-  checkColPlacement(puzzleString, row, column, value) {}
+  checkColPlacement(puzzleString, row, column, value) {
+    let targetColumn = this.getTargetColumn(puzzleString, column);
+    let isPresent = targetColumn.includes(value);
+    return !isPresent;
+  }
 
-  checkRegionPlacement(puzzleString, row, column, value) {}
+  getTargetColumn(puzzleString, column) {
+    let targetColumn = "";
+    for (let i = 0; i < this.numberOfCharacters; i++) {
+      if (i % 9 == column - 1) {
+        targetColumn = targetColumn + puzzleString[i];
+      }
+    }
+    return targetColumn;
+  }
+
+  checkRegionPlacement(puzzleString, row, column, value) {
+    let targetRegion = getTargetRegion();
+  }
 
   solve(puzzleString) {}
 }
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,10 +1,16 @@
 const chai = require("chai");
 const assert = chai.assert;
+const { puzzlesAndSolutions } = require("../controllers/puzzle-strings.js");
 
 const Solver = require("../controllers/sudoku-solver.js");
 let solver = new Solver();
 
 suite("Unit Tests", () => {
+  function getSample() {
+    return puzzlesAndSolutions[
+      Math.floor(Math.random() * puzzlesAndSolutions.length)
+    ][0];
+  }
   function createRandomString(length, valid) {
     let chars;
     let result = "";
@@ -12,12 +18,12 @@ suite("Unit Tests", () => {
     if (valid) {
       chars = ".123456789";
     } else {
-      chars = ",#+)(}{][|'";
+      chars = ",#+)(}{][|abcd'";
     }
     for (let i = 0; i < length; i++) {
       result += chars.charAt(Math.floor(Math.random() * chars.length));
     }
-    console.log("result:", result);
+    // console.log("result:", result);
     return result;
   }
   suite("string validation", () => {
@@ -32,15 +38,36 @@ suite("Unit Tests", () => {
       assert.equal(solver.validate(createRandomString(80, false)), "NO!");
     });
   });
+  suite("Placement Validation", () => {
+    test("Logic handles a valid row placement", () => {
+      assert.equal(
+        solver.checkRowPlacement(puzzlesAndSolutions[0][0], "A", 2, 3),
+        true
+      );
+    });
+    test("Logic handles an invalid row placement", () => {
+      assert.equal(
+        solver.checkRowPlacement(puzzlesAndSolutions[0][0], "B", 1, 6),
+        false
+      );
+    });
+    test("Logic handles a valid column placement", () => {
+      assert.equal(
+        solver.checkColPlacement(puzzlesAndSolutions[0][0], "B", 1, 5),
+        true
+      );
+    });
+    test("Logic handles an invalid column placement", () => {
+      assert.equal(
+        solver.checkColPlacement(puzzlesAndSolutions[0][0], "B", 1, 1),
+        false
+      );
+    });
+  });
 });
 
 /**
  * 
-
-Logic handles a valid row placement
-Logic handles an invalid row placement
-Logic handles a valid column placement
-Logic handles an invalid column placement
 Logic handles a valid region (3x3 grid) placement
 Logic handles an invalid region (3x3 grid) placement
 Valid puzzle strings pass the solver
